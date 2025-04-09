5439c95f813e838ac001d2fb456016bc8bef8c1b
Daxter <danieljurado147@gmail.com>
2025-04-09 17:49:56 +0200

    topito

--- game_manager.gd
+++ game_manager.gd
@@ -3,7 +3,7 @@ extends Node
 var vida_personaje: int = 100
 var vida_personaje_maxima: int = 100
 var ataque_personaje: float = 1.0
-var velocidad_personaje: float = 170.0
+var velocidad_personaje: float = 300.0
 var defensa_personaje: float = 1.0
 var cooldown_disparo: float = 1.0
 var cooldown_garra: float = 0.5
